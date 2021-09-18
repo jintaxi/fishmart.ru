949e909084c0d1a095bf7ba2717cde61c0876b73
jintaxi <jintaxi97@gmail.com>
2021-09-19 00:11:08 +0300

    Добавлены комментарии

--- sender.py
+++ sender.py
@@ -2,8 +2,11 @@ import smtplib
 import os
 from email.mime.text import MIMEText
 
+
+# Пользователи для рассылки
 users = ['jintaxi97@gmail.com', 'evn06@mail.ru', 'veduser@mail.ru']
 
+# Инициализация почтового клиента
 password = os.getenv("EMAIL_PASSWD")
 sender = 'jintaxi.mailing@gmail.com'
 server = smtplib.SMTP("smtp.gmail.com")
@@ -11,9 +14,10 @@ server = smtplib.SMTP("smtp.gmail.com")
 server.starttls()
 
 
-# Оповещение
+# Отправка сообщения
 def send_mail(mail):
 
+    # Генерация сообщения
     mail = MIMEText(f"{mail}")
     mail['Subject'] = "Отслеживание рыбы"  
 
